fe0356570dcf4dbb709711847688b7f4a197eb07
Cathy Li <fangyicathy@gmail.com>
2025-07-24 05:01:02 +0000

    Add

--- listener.py
+++ listener.py
@@ -70,6 +70,8 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
         df = pd.DataFrame(rows, columns=['chain', 'token', 'recipient', 'amount', 'transactionHash', 'address'])
         df.to_csv('deposit_logs.csv', index=False)
     else:
+        pd.DataFrame(columns=['chain', 'token', 'recipient', 'amount', 'transactionHash', 'address']).to_csv('deposit_logs.csv', index=False)
+
         for block_num in range(start_block,end_block+1):
             event_filter = contract.events.Deposit.create_filter(from_block=block_num,to_block=block_num,argument_filters=arg_filter)
             events = event_filter.get_all_entries()
